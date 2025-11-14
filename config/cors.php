480260448887d0a24a98ffd3b27d0329660b7142
elexispd <promisedeco24@gmail.com>
2025-11-14 10:31:59 +0100

    update cors to avoid cor issue with react

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://coafcare.online'
+    ],
 
     'allowed_origins_patterns' => [],
 
